8a372618a1131d693b72a045af25cd3785217fad
Ferdinandio <dmi8829@yandex.ru>
2020-03-26 17:27:05 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+
+// var number = 5;
+// var string = 'Hello!';
+// var sym = Symbol();
+// var boolean = true;
+// undefined;
+// null;    // 
+// var obj = {};
+
+// console.log(4/0);
+
+// console.log('string'*5);
+
+
+// let person = {
+//   name: 'John',
+//   age: 25,
+//   isMarried: false
+// };
+
+// console.log(person["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]);
+
+// // alert('Hello, World!');
+
+// // let answer = confirm('Are you here?');
+
+// // console.log(answer);
+
+// let answer = +prompt('Do you have 18 years old?', 'Yes');
+
+// console.log(typeof(answer));
+
+// console.log(5 + +'- object');
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log('2' == 2);
+console.log('2' === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+console.log(isChecked && isClose);
+console.log(isChecked && !isClose);
+
+
+
+
+
+
+
